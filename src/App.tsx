b873acb28f1dfd90e1cdb5256c13f8e5af4b5001
Adam Skrzypinski <adamskrzy@gmail.com>
2023-10-27 20:15:01 +0200

    Warehouse component and React Links added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import './App.scss';
-
-import {Header} from "./components/layout/Header";
-import {MainPage} from "./components/Main-page/Main-page";
+import React from "react";
+import "./App.scss";
+import { Header } from "./components/layout/Header";
+import { MainPage } from "./components/Main-page/Main-page";
+import { Route, Routes } from "react-router-dom";
+import { Warehouse } from "./components/Warehouse/Warehouse";
 
 export const App = () => {
-    return (
-        <div className={'app'}>
-            <Header/>
-            <MainPage/>
-
-
-
-        </div>
-    );
-}
+  return (
+    <div className={"app"}>
+      <Header />
+      <Routes>
+        <Route path={"/"} element={<MainPage />} />
+        <Route path={"/warehouse"} element={<Warehouse />} />
+      </Routes>
+    </div>
+  );
+};
--- src/components/Main-page/Main-page.tsx
+++ src/components/Main-page/Main-page.tsx
@@ -1,21 +1,45 @@
 import React from "react";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faBarcode, faPlusMinus, faWarehouse} from "@fortawesome/free-solid-svg-icons";
-import './Main-page.scss'
-import {MainPageItems} from "./Main-page-items/Main-page-items";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBarcode,
+  faPlusMinus,
+  faWarehouse,
+} from "@fortawesome/free-solid-svg-icons";
+import "./Main-page.scss";
+import { MainPageItems } from "./Main-page-items/Main-page-items";
+import { Link } from "react-router-dom";
 
 export const MainPage = () => {
-    return (<div className={'home-page'}>
-
-        <MainPageItems text={'Dodaj produkt'} icon={<FontAwesomeIcon
-            icon={faBarcode} size={'10x'}/>} iconCount={2}/>
-        <MainPageItems text={'Dodaj lokalizację'} icon={<FontAwesomeIcon
-            icon={faWarehouse} size={'10x'}/>} iconCount={2}/>
-        <MainPageItems text={'Magazyn'} icon={<FontAwesomeIcon
-            icon={faWarehouse} size={'10x'}/>} iconCount={1}/>
-        <MainPageItems text={'Inwentaryzuj'} icon={<FontAwesomeIcon icon={faPlusMinus} size={'10x'}/>} iconCount={1}/>
-
-
-
-   </div>)
-}
+  return (
+    <div className={"home-page"}>
+      <Link to={"/add-product"}>
+        <MainPageItems
+          text={"Dodaj produkt"}
+          icon={<FontAwesomeIcon icon={faBarcode} size={"10x"} />}
+          iconCount={2}
+        />
+      </Link>
+      <Link to={"/add-location"}>
+        <MainPageItems
+          text={"Dodaj lokalizację"}
+          icon={<FontAwesomeIcon icon={faWarehouse} size={"10x"} />}
+          iconCount={2}
+        />
+      </Link>
+      <Link to={"/warehouse"}>
+        <MainPageItems
+          text={"Magazyn"}
+          icon={<FontAwesomeIcon icon={faWarehouse} size={"10x"} />}
+          iconCount={1}
+        />
+      </Link>
+      <Link to={"/inventory"}>
+        <MainPageItems
+          text={"Inwentaryzuj"}
+          icon={<FontAwesomeIcon icon={faPlusMinus} size={"10x"} />}
+          iconCount={1}
+        />
+      </Link>
+    </div>
+  );
+};
