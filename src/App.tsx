780b99b2adc5cf57a7d615144666eb35cedf354c
Adam Skrzypinski <adamskrzy@gmail.com>
2023-11-06 20:02:16 +0100

    Add-location view added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,39 @@
 import React from "react";
 import "./App.scss";
-import {Header} from "./components/layout/Header";
-import {MainPageView} from "./views/Main-page-view/Main-page-view";
-import {Route, Routes} from "react-router-dom";
-import {WarehouseView} from "./views/Warehouse-view/Warehouse-view";
-import {SingleAreaView} from "./views/Warehouse-view/Single-area-view/Single-area-view";
-import {NotFoundView} from "./views/Not-found-view/Not-found-view";
-import {SingleProductView} from "./views/Product-view/Single-product-view/Single-product-view";
-import {SinglePlaceView} from "./views/Warehouse-view/Single-place-view/Single-place-view";
-import {DeleteProductView} from "./views/Warehouse-view/Delete-product-view/Delete-product-view";
-import {AddProductView} from "./views/Product-view/Add-product-view/Add-product-view";
-import {AddConfirmView} from "./views/Product-view/Add-confirm-view/Add-confirm-view";
-import {EditProductView} from "./views/Product-view/Edit-product-view/Edit-product-view";
+import { Header } from "./components/layout/Header";
+import { MainPageView } from "./views/Main-page-view/Main-page-view";
+import { Route, Routes } from "react-router-dom";
+import { WarehouseView } from "./views/Warehouse-view/Warehouse-view";
+import { SingleAreaView } from "./views/Warehouse-view/Single-area-view/Single-area-view";
+import { NotFoundView } from "./views/Not-found-view/Not-found-view";
+import { SingleProductView } from "./views/Product-view/Single-product-view/Single-product-view";
+import { SinglePlaceView } from "./views/Warehouse-view/Single-place-view/Single-place-view";
+import { DeleteProductView } from "./views/Warehouse-view/Delete-product-view/Delete-product-view";
+import { AddProductView } from "./views/Product-view/Add-product-view/Add-product-view";
+import { AddConfirmView } from "./views/Product-view/Add-confirm-view/Add-confirm-view";
+import { EditProductView } from "./views/Product-view/Edit-product-view/Edit-product-view";
+import { AddLocation } from "./views/Warehouse-view/Add-location-view/Add-location";
 
 export const App = () => {
-    return (
-        <div className={"app"}>
-            <Header/>
-            <Routes>
-                <Route path={"/"} element={<MainPageView/>}/>
-                <Route path={"/warehouse"} element={<WarehouseView/>}/>
-                <Route path={"/warehouse/area/:areaId"} element={<SingleAreaView/>}/>
-                <Route
-                    path={"/warehouse/place/:placeId"}
-                    element={<SinglePlaceView/>}
-                />
-                <Route path={"/warehouse/:productId"} element={<SingleProductView/>}/>
-                <Route
-                    path={"/add-product"} element={<AddProductView/>}
-                />
-                <Route
-                    path={"/add-product/confirm"} element={<AddConfirmView/>}
-                />
-                <Route
-                    path={"product/edit/:productId"} element={<EditProductView/>}
-                />
+  return (
+    <div className={"app"}>
+      <Header />
+      <Routes>
+        <Route path={"/"} element={<MainPageView />} />
+        <Route path={"/warehouse"} element={<WarehouseView />} />
+        <Route path={"/warehouse/area/:areaId"} element={<SingleAreaView />} />
+        <Route
+          path={"/warehouse/place/:placeId"}
+          element={<SinglePlaceView />}
+        />
+        <Route path={"/warehouse/:productId"} element={<SingleProductView />} />
+        <Route path={"/add-product"} element={<AddProductView />} />
+        <Route path={"/add-product/confirm"} element={<AddConfirmView />} />
+        <Route path={"product/edit/:productId"} element={<EditProductView />} />
+        <Route path={"add-location"} element={<AddLocation />} />
 
-                <Route path="*" element={<NotFoundView/>}/>
-            </Routes>
-        </div>
-    );
+        <Route path="*" element={<NotFoundView />} />
+      </Routes>
+    </div>
+  );
 };
--- src/views/Product-view/Add-product-view/Add-product-view.tsx
+++ src/views/Product-view/Add-product-view/Add-product-view.tsx
@@ -1,130 +1,130 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "./Add-product-view.scss";
-import {CreateProductDto} from "../../../types/product";
-import {Spinner} from "../../../components/common/Spinner/Spinner";
-import {apiUrl} from "../../../config/api";
-import {LocationSelect} from "../../../components/Warehouse/Location-select/Location-select";
+import { CreateProductDto } from "../../../types/product";
+import { Spinner } from "../../../components/common/Spinner/Spinner";
+import { apiUrl } from "../../../config/api";
+import { LocationSelect } from "../../../components/Warehouse/Location-select/Location-select";
 
 export const AddProductView = () => {
-    const [product, setProduct] = useState<CreateProductDto>({
-        name: "",
-        measure: "",
-        count: 0,
-    });
-    const [loading, setLoading] = useState<boolean>(false);
+  const [product, setProduct] = useState<CreateProductDto>({
+    name: "",
+    measure: "",
+    count: 0,
+  });
+  const [loading, setLoading] = useState<boolean>(false);
 
-    const [btnDisabled, setBtnDisabled] = useState<boolean>(true);
+  const [btnDisabled, setBtnDisabled] = useState<boolean>(true);
 
-    const [newProductId, setNewProductId] = useState<string | null>(null);
+  const [newProductId, setNewProductId] = useState<string | null>(null);
 
-    console.log(newProductId)
-    const sendForm = async (e: React.SyntheticEvent) => {
-        e.preventDefault();
-        setLoading(true);
-        try {
-            const res = await fetch(`${apiUrl}/product`, {
-                method: "POST",
-                headers: {"Content-Type": "application/json"},
-                body: JSON.stringify(product),
-            });
-            const data = await res.json();
-            if (data.id !== undefined) {
-                setNewProductId(data.id)
-                setLoading(false)
-            }
-        } finally {
-            setLoading(false);
-        }
-    };
+  const sendForm = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const res = await fetch(`${apiUrl}/product`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(product),
+      });
+      const data = await res.json();
+      if (data.id !== undefined) {
+        setNewProductId(data.id);
 
-    const change = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setProduct((product) => ({
-            ...product,
-            [e.target.name]: e.target.value,
-        }));
-    };
-
-    useEffect(() => {
-        if (
-            product.name.length > 2 &&
-            product.name.length < 70 &&
-            product.count !== '' &&
-            product.count < 999999 &&
-            product.measure.length > 1 &&
-            product.measure.length < 15
-        ) {
-            setBtnDisabled(false);
-        } else {
-            setBtnDisabled(true);
-        }
-    }, [product]);
-
-    if (loading) {
-        return <Spinner/>;
+        setLoading(false);
+      }
+    } finally {
+      setLoading(false);
     }
+  };
 
-    return (
-        <>
-            <div className={"add-product"}>
-                <h2>Dodaj produkt</h2>
-                <div
-                    className={"disabled-area"}
-                    style={{display: newProductId ? "flex" : "none"}}
-                >
-                    <h3>ZAPISANO!</h3>
-                </div>
-                <form onSubmit={sendForm}>
-                    <label>
-                        <p>Nazwa:</p>
-                        <input
-                            type={"text"}
-                            name={"name"}
-                            value={product.name}
-                            onChange={change}
-                            placeholder={"Podaj nazwę"}
-                            minLength={3}
-                            maxLength={70}
-                        />
-                        <p className={"info"}>Od 3 do 70 znaków</p>
-                    </label>
-                    <label>
-                        <p> Ilość:</p>
-                        <input
-                            type={"number"}
-                            name={"count"}
-                            value={product.count}
-                            onChange={change}
-                            placeholder={"Podaj ilość"}
-                            max={999999}
-                        />
-                        <p className={"info"}>Od 0 do 999999</p>
-                    </label>
-                    <label>
-                        <p>Jednostka:</p>
-                        <input
-                            type={"text"}
-                            name={"measure"}
-                            value={product.measure}
-                            onChange={change}
-                            placeholder={"szt, m3, kg etc."}
-                            minLength={2}
-                            maxLength={15}
-                        />
-                        <p className={"info"}>Od 2 do 15 znaków</p>
-                    </label>
+  const change = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setProduct((product) => ({
+      ...product,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
-                    <button
-                        disabled={btnDisabled}
-                        className={btnDisabled ? "disabled" : ""}
-                        type={"submit"}
-                    >
-                        Zapisz
-                    </button>
-                </form>
-            </div>
-            <div className={"location-select"}>
-                <LocationSelect product={product} newProductId={newProductId}/>
-            </div>
-        </>
-    );
+  useEffect(() => {
+    if (
+      product.name.length > 2 &&
+      product.name.length < 70 &&
+      product.count !== "" &&
+      product.count < 999999 &&
+      product.measure.length > 1 &&
+      product.measure.length < 15
+    ) {
+      setBtnDisabled(false);
+    } else {
+      setBtnDisabled(true);
+    }
+  }, [product]);
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  return (
+    <>
+      <div className={"add-product"}>
+        <h2>Dodaj produkt</h2>
+        <div
+          className={"disabled-area"}
+          style={{ display: newProductId ? "flex" : "none" }}
+        >
+          <h3>ZAPISANO!</h3>
+        </div>
+        <form onSubmit={sendForm}>
+          <label>
+            <p>Nazwa:</p>
+            <input
+              type={"text"}
+              name={"name"}
+              value={product.name}
+              onChange={change}
+              placeholder={"Podaj nazwę"}
+              minLength={3}
+              maxLength={70}
+            />
+            <p className={"info"}>Od 3 do 70 znaków</p>
+          </label>
+          <label>
+            <p> Ilość:</p>
+            <input
+              type={"number"}
+              name={"count"}
+              value={product.count}
+              onChange={change}
+              placeholder={"Podaj ilość"}
+              max={999999}
+            />
+            <p className={"info"}>Od 0 do 999999</p>
+          </label>
+          <label>
+            <p>Jednostka:</p>
+            <input
+              type={"text"}
+              name={"measure"}
+              value={product.measure}
+              onChange={change}
+              placeholder={"szt, m3, kg etc."}
+              minLength={2}
+              maxLength={15}
+            />
+            <p className={"info"}>Od 2 do 15 znaków</p>
+          </label>
+
+          <button
+            disabled={btnDisabled}
+            className={btnDisabled ? "disabled" : ""}
+            type={"submit"}
+          >
+            Zapisz
+          </button>
+        </form>
+      </div>
+      <div className={"location-select"}>
+        <LocationSelect product={product} newProductId={newProductId} />
+      </div>
+    </>
+  );
 };
